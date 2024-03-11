04fc14d7a39240ea16e7c146c8510c33f5ceec35
dan1hc <dan@1howardcapital.com>
2024-03-10 18:07:15 -0700

    docs: __coc_pr_template__
    add code of conduct and pr template
    
    resolves #24

---
+++ .github/actions/validate-commit-message/index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const pattern = /^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)(\(\w+\))?((?=:\s)|(?=!:\s))?(!)?(:\s\_\_.*\_\_)($|( *\n\n)(.+)?(\n\n)((resolve[ds]? \#\d+|fix(ed|es)? \#\d+|close[ds]? \#\d+)(, )?)+$)/g;
+
+if (github.context.payload.event.commits.length >= 20) {
+    core.setFailed("Pull request must contain fewer than 20 commits.");
+} else {
+    github.context.payload.event.commits.every((commit) => {
+        if (commit.author.name == 'actions-user' || pattern.test(commit.message)) {
+            console.log(`VALID: ${commit.message}`)
+            return true
+        } else {
+            core.setFailed(`INVALID: ${commit.message}`)
+            return false
+        }
+    });
+};
