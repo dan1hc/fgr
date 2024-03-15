0f8c9ecdfd617f39c4aaf13e4fd3fdb16bf9dced
dan1hc <dan@1howardcapital.com>
2024-03-15 10:14:22 -0700

    ci: __commmit_msg_parse_fix__
    minor parsing fix II
    
    resolve #6, fix #26

--- .github/actions/validate-commit-message/index.js
+++ .github/actions/validate-commit-message/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const pattern = /^(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test)(\(\w+\))?((?=:\s)|(?=!:\s))?(!)?(:\s\_\_.*\_\_)($|( *\n\n)(.+)?(\n\n)((resolve[ds]? \#\d+|fix(ed|es)? \#\d+|close[ds]? \#\d+)(, )?)+$)/g;
 
-const commits = JSON.parse(github.context.payload.event).commits;
+const commits = JSON.parse(github.context.payload).event.commits;
 
 if (commits.length >= 20) {
     core.setFailed("Pull request must contain fewer than 20 commits.");
